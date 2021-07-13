40facf0e6971a32e1fab17fe69b73980f548885c
Riza <satiseven777@gmail.com>
2021-07-13 23:01:50 +0300

    adding urql as graphql client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
-import theme from '../theme'
-import { AppProps } from 'next/app'
-
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider, createClient } from "urql";
+import theme from "../theme";
+import { AppProps } from "next/app";
+const client = createClient({
+  url: `http://localhost:4000/graphql`,
+  fetchOptions: {
+    credentials: "include",
+  },
+});
+console.log(process.env.GRAPHQL_SERVER);
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -10,25 +10,53 @@ import { Form, Formik } from "formik";
 import React from "react";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
-
+import { useMutation } from "urql";
 interface RegisterProps {}
 
 const Register: React.FC<RegisterProps> = ({}) => {
+  const [, register] = useMutation(`
+  mutation Register($username:String!,$password:String!,$name:String!,$email:String!){
+    register(options:{name:$username,email:$email,username:$username,password:$password
+    }){
+      errors{
+        field
+        message
+      }, 
+      user{
+        email
+        id
+      }
+    }
+  }
+  `);
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ username: "", password: "", email: "", name: "" }}
         onSubmit={(values) => {
-          console.log(values);
+          return register(values);
         }}
       >
         {({ errors, values, isSubmitting, handleChange }) => (
           <Form>
-            <InputField
-              name="username"
-              placeholder="username"
-              label="Username"
-            />
+            <InputField name="name" placeholder="Name" label="Name" />
+
+            <Box mt={4}>
+              <InputField
+                type="email"
+                name="email"
+                placeholder="Email"
+                label="Email"
+              />
+            </Box>
+            <Box mt={4}>
+              <InputField
+                type="text"
+                name="username"
+                placeholder="Username"
+                label="Username"
+              />
+            </Box>
             <Box mt={4}>
               <InputField
                 type="password"
